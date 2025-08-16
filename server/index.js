6211096a1ac842c2c091c8e249812c2313f6fc75
areimo <areimo011@gmail.com>
2025-08-15 23:13:08 -0300

    update server/index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ if (!process.env.MP_ACCESS_TOKEN) {
   process.exit(1);
 }
 
-mercadopago.configurations.setAccessToken(process.env.MP_ACCESS_TOKEN);
+new MercadoPagoConfig({
+  accessToken: process.env.MP_ACCESS_TOKEN,
+  options: { timeout: 5000 },
+});
+
 
 const app = express();
 
