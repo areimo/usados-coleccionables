551a1e58febf5c46fe8ef132b53ff9003ba9f789
areimo <areimo011@gmail.com>
2025-08-19 09:14:43 -0300

    updated App.css and wppContact.jsx

--- src/wppContact.jsx
+++ src/wppContact.jsx
@@ -6,8 +6,7 @@ const WppContact = () => {
   const message = "Hola, estoy interesado en un service...";
   const wppLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
 
-  const [size, setSize] = useState(75);
-  const [rightPos, setRightPos] = useState(20);
+  const [size, setSize] = useState(75); // tamaño en px
 
   useEffect(() => {
     const updateSize = () => {
@@ -15,13 +14,10 @@ const WppContact = () => {
 
       if (width <= 600) { // celular
         setSize(50);
-        setRightPos(40);
       } else if (width <= 1024) { // tablet
         setSize(60);
-        setRightPos(35);
       } else { // PC
         setSize(75);
-        setRightPos(20);
       }
     };
 
@@ -41,8 +37,8 @@ const WppContact = () => {
           cursor: "pointer",
           transition: "transform 0.2s ease-in-out",
           position: "fixed",
-          right: `${rightPos}px`,
-          bottom: "5rem",
+          right: "1rem",
+          bottom: "1rem", // ahora pegado al borde inferior
           zIndex: 1000,
         }}
         onMouseOver={e => e.currentTarget.style.transform = "scale(1.1)"}
@@ -53,6 +49,7 @@ const WppContact = () => {
 };
 
 export default WppContact;
+
 
 
 
