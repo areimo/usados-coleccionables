b5a98bdd2ae2785a8f902b09e35c24077368ee55
areimo <areimo011@gmail.com>
2025-08-19 13:13:50 -0300

    updated slider.jsx

--- src/slider.jsx
+++ src/slider.jsx
@@ -18,8 +18,8 @@ const AutoSlider = () => {
 
       if (width <= 600) { // celular
         setVisibleCount(1);
-        setImageWidth(width * 0.8);
-        setImageHeight((width * 0.8));
+        setImageWidth(width * 2);
+        setImageHeight((width * 2));
       } else if (width <= 1024) { // tablet
         setVisibleCount(2);
         setImageWidth(250);
